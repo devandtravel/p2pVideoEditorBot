926ab8276ec3b2ed0ae3fe5de808577510b789ff
devandtravel <devandtravel@gmail.com>
2021-12-01 15:23:34 +0300

    start bot is ok

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,18 @@
 import { Bot } from 'https://deno.land/x/grammy/mod.ts'
 import 'https://deno.land/x/dotenv/load.ts'
 
-const bot = new Bot(Deno.env.get('BOT_TOKEN'))
+const TOKEN = Deno.env.get('BOT_TOKEN')
 
-bot.command('start', ctx => ctx.reply('Welcome! Up and running.'))
+if (TOKEN === undefined) {
+  throw new Error('BOT_TOKEN must be provided! BOT_TOKEN is undefined.')
+}
+const bot = new Bot(TOKEN)
+
+bot.command('start', ctx =>
+  ctx.reply(
+    'Привет! Готов помочь тебе с монтажом свадебных фильмов. Ответь на несколько вопросов.'
+  )
+)
 bot.on('message', ctx => ctx.reply('Got another message!'))
 
 bot.start()
