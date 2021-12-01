a2ed50f89a885509f68900620ded404e1556438c
devandtravel <devandtravel@gmail.com>
2021-12-01 17:46:52 +0300

    bot starts

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,29 @@
-import { Bot } from 'https://deno.land/x/grammy/mod.ts'
-import 'https://deno.land/x/dotenv/load.ts'
-
-const TOKEN = Deno.env.get('BOT_TOKEN')
-
-if (TOKEN === undefined) {
-  throw new Error('BOT_TOKEN must be provided! BOT_TOKEN is undefined.')
+/* #region  IMPORTS */
+import dotenv from 'dotenv'
+import { Bot } from 'grammy'
+import { testOrders } from './models/testOrders.js'
+/* #endregion */
+/* #region  SETUP ENVIROMENT */
+dotenv.config()
+/* #endregion */
+/* #region  INITIALIZE BOT */
+const BOT_TOKEN = process.env.BOT_TOKEN
+if (BOT_TOKEN === undefined) {
+  throw new TypeError('BOT_TOKEN must be provided! BOT_TOKEN is undefined.')
 }
-const bot = new Bot(TOKEN)
-
+const bot = new Bot(BOT_TOKEN)
+/* #endregion */
+/* #region  INITIALIZE DATABASE */
+const orders = testOrders
+/* #endregion */
 bot.command('start', ctx =>
   ctx.reply(
     'Привет! Готов помочь тебе с монтажом свадебных фильмов. Ответь на несколько вопросов.'
   )
 )
-bot.on('message', ctx => ctx.reply('Got another message!'))
-
+bot.on('message', ctx =>
+  ctx.reply('Я не знаю, что ответить на такое сообщение.')
+)
+/* #region  START BOT */
 bot.start()
+/* #endregion */
